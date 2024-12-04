d619d10465271017cf04514b3b27dd6ebd12a346
proboscis <nameissoap@gmail.com>
2024-12-05 03:38:09 +0900

    hands-down-gold with combo

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/handsdown_gold/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/handsdown_gold/keymap.c
@@ -20,24 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-const uint16_t PROGMEM yw_is_wh[] = {KC_Y, KC_W, COMBO_END};
-const uint16_t PROGMEM jg_is_z[] = {KC_J, KC_G, COMBO_END};
-const uint16_t PROGMEM gp_is_qu[] = {KC_G, KC_P, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(yw_is_wh, SEND_STRING("WH")), // i want to send "WH"
-    COMBO(jg_is_z, KC_Z), 
-    COMBO(gp_is_qu, KC_Q)
-};
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-      KC_J     , KC_Z     , KC_G     , KC_Q           , KC_M      ,                 KC_P        , KC_V     , KC_SCLN  , KC_COLN  , KC_DOT   ,
-      KC_R     , KC_S     , KC_N     , KC_D           , KC_B      ,                 KC_COMM     , KC_I     , KC_A     , KC_E     , KC_H     ,
-      KC_X     , KC_F     , KC_L     , KC_C           , KC_K      ,                 KC_MINS     , KC_T     , KC_U     , KC_O     , KC_W     ,
-      KC_LCTL  , KC_LGUI  , KC_LALT  , LSFT_T(KC_LNG2), KC_T      , LT(3,KC_LNG1) , LT(1,KC_SPC),KC_BSPC   ,LSFT_T(KC_LNG2),KC_RALT,KC_RGUI,KC_RSFT
-    ),
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+  ),
 
   [1] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
